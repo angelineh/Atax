2c1825f4cd8d7b948ec393577ce515ca115cb347
benallamar <marouan.benalla@gmail.com>
2016-01-05 20:33:24 +0000

    Have some visualisation to see

--- src/Interface.java
+++ src/Interface.java
@@ -12,29 +12,34 @@ public class Interface extends JPanel implements MouseListener{
 	public Interface(Plateau plateau,int positionY){
 		this.plateau = plateau;
 		this.positionY = positionY;
-		
+
 	}
 	public void paintComponent(Graphics g){
 		for(int i = 0; i < plateau.largeur ; i++){
 			for(int j=0;j<plateau.largeur;j++){
 				g.drawImage(this.plateau.base_material, i*50, j*50, this);
+				if(plateau.wait){
+					g.drawImage(this.plateau.selected_image, plateau.selected[0]*50,plateau.selected[1]*50, this);
+				}
 				if(plateau.positions[i][j]== 1){
 					g.drawImage(this.plateau.white_side, i*50, j*50, this);
 				}
 				if(plateau.positions[i][j]== 2){
 					g.drawImage(this.plateau.black_side, i*50, j*50, this);
 				}
+
 			}
 		}
 	}
 
 	public void mouseClicked(MouseEvent e) {
-		// TODO Auto-generated method stub
-		System.out.println(e.getY()+ "   " + e.getX() + " " + this.positionY);
 		int absoluteY = (e.getY()-positionY) / 50, absoluteX = e.getX()/50;
-		this.plateau.positions[absoluteX][absoluteY]=this.plateau.role?1:2;
-		this.plateau.role = !this.plateau.role;
-		this.repaint();
+		if(plateau.nextMove(absoluteX, absoluteY)){
+			this.repaint();
+		}else{
+			System.out.println("your move is not correct, please chose an other direction");
+			this.repaint();
+		}
 	}
 
 	public void mousePressed(MouseEvent e) {
--- src/Plateau.java
+++ src/Plateau.java
@@ -10,19 +10,26 @@ public class Plateau{
 	public BufferedImage black_side;
 	public BufferedImage white_side;
 	public BufferedImage base_material;
+	public BufferedImage selected_image;
+	public BufferedImage wrong_image;
 	public Boolean role = false;
 	public Integer hauteur;
 	public Integer largeur;
+	public Boolean wait = false;// ce boolean veut dire que l'utilisateur à choisi le point qu'il veut bouger et qu'il attend à ce qu'il le bouge
 	public int[][] positions;
-	
+	public int[] selected = new int[2];
+
+
 	public Plateau(int hauteur,int largeur){
 		this.hauteur = hauteur;
 		this.largeur = largeur;
 		this.positions = new int[hauteur][largeur];
 		try {
-		    this.black_side = ImageIO.read(new File("src/media/black.png"));
-		    this.white_side = ImageIO.read(new File("src/media/white.png"));
-		    this.base_material = ImageIO.read(new File("src/media/baseMetal.png"));
+			this.black_side = ImageIO.read(new File("src/media/black.png"));
+			this.white_side = ImageIO.read(new File("src/media/white.png"));
+			this.base_material = ImageIO.read(new File("src/media/baseMetal.png"));
+			this.selected_image = ImageIO.read(new File("src/media/selected.png"));
+			this.wrong_image = ImageIO.read(new File("src/media/wrong.png"));
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
 			System.out.print("Some images file are missings, please check "
@@ -44,9 +51,64 @@ public class Plateau{
 		positions[0][largeur-1]=2;
 		positions[hauteur-1][0]=2;
 	}
-	public void AfficherCaseDisponible(){
+	public Boolean nextMove(int positionX,int positionY){
+		if(wait){
+			if(this.updateTheVue(positionX, positionY)){
+				this.role = !this.role;
+				this.wait =! this.wait;
+				return true;
+			}else{
+				
+				return false;
+			}
 
+		}else{
+			if(this.setSelected(positionX, positionY)){
+				this.wait =! this.wait;
+				return true;
+			}else{
+				return false;
+			}
+		}
 	}
-
+	public Boolean updateTheVue(int x, int y ){
+		// c'est ce vue qui doit s'occuper de l'update des vues,
+		if(Math.max(Math.abs(selected[0]-x), Math.abs(selected[1]-y))==1){
+			this.setVoisin(role?1:2,x,y);
+			return true;
+		}else if(Math.abs(selected[0]-x)==2 && Math.abs(selected[1]-y)==2 ){
+			this.setVoisin(role?1:2,x,y);
+			positions[selected[0]][selected[1]]=0;
+			return true;
+		}else{
+			return false;
+		}
+	}
+	public Boolean setSelected(int x, int y){
+		if(this.hasPermissionToPlay(x, y)){
+			this.selected[0]=x;this.selected[1]=y;
+			return true;
+		}
+		return false;
+	}
+	public Boolean hasPermissionToPlay(int x,int y){
+		return role && positions[x][y]==1 || !role && positions[x][y]== 2;
+	}
+	public void setVoisin(int value, int x, int y){
+		int X = x-1, Y=y-1;
+		positions[x][y]=value;
+		for(int i = 0; i < 2; i++){
+			for(int j=0; j<2;j++){
+				if(this.inPlateau(X+i, Y+j)){
+					if(positions[X+i][Y+j]>0){
+						positions[X+i][Y+j]=value;
+					}
+				}
+			}
+		}
+	}
+	public Boolean inPlateau(int x, int y){
+		return x >=0 && x < largeur && y >=0 && y < hauteur;
+	}
 }
 
